b4a3815fd868406ce171ffa91f0be3af950ab5a3
Razafimahefa Maresssah <maressahrazafy@gmail.com>
2026-02-25 20:17:50 +0300

    Update backend

--- Express/src/utils/password.util.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-exports.hasher = async (motDePasse) => {
-  return await bcrypt.hash(motDePasse, 10);
-};
-
-exports.comparer = async (motDePasse, hash) => {
-  return await bcrypt.compare(motDePasse, hash);
-};
