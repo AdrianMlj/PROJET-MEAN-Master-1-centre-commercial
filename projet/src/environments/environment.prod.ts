32501c69c0d46fc309cf309840ff80175b88c2e1
Razafimahefa Maresssah <maressahrazafy@gmail.com>
2026-02-27 18:48:45 +0300

    Update environment.prod.ts

--- projet/src/environments/environment.prod.ts
+++ projet/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://projet-mean-master-1-centre-commercial-1.onrender.com/api',
+  apiUrl: 'https://projet-mean-master-1-centre-commercial.onrender.com/api',
   roles: {
     admin: 'admin_centre',
     boutique: 'boutique',
