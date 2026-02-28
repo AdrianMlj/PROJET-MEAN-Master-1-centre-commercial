326ba063ac54cb8271d0e148edbffe13674efec1
Razafimahefa Maresssah <maressahrazafy@gmail.com>
2026-02-28 17:00:51 +0300

    Update environment.prod.ts

--- projet/src/environments/environment.prod.ts
+++ projet/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://m1p13mean-adrianno-maressah.onrender.com',
+  apiUrl: 'https://m1p13mean-adrianno-maressah.onrender.com/api',
   roles: {
     admin: 'admin_centre',
     boutique: 'boutique',
